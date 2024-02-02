6bd1473c3a2eecd9ea7a90392ea74e7478822d25
hoangbao0201 <hoangbao020103@gmail.com>
2024-02-02 12:26:01 +0700

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors();
+
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     app.useGlobalPipes(new ValidationPipe());
     app.use(json({ limit: '50mb' }));
--- src/novel/novel.controller.ts
+++ src/novel/novel.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { NovelService } from './novel.service';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('/api/novels')
 export class NovelController {
   constructor(private readonly novelService: NovelService) {}
 
   @Get()
+  @ApiQuery({ name: 'q', required: false, type: String, description: 'Query string for search' })
+  @ApiQuery({ name: 'bya', required: false, type: String, description: 'Another query parameter' })
+  @ApiQuery({ name: 'take', required: false, type: Number, description: 'Number of items to take' })
+  @ApiQuery({ name: 'skip', required: false, type: Number, description: 'Number of items to skip' })
+  @ApiQuery({ name: 'sort', required: false, enum: ['desc', 'asc'], description: 'Sort order' })
   findAll(
-    @Query('q') q: string,
-    @Query('bya') bya: string,
-    @Query('take') take: number,
-    @Query('skip') skip: number,
-    @Query('sort') sort: 'desc' | 'asc',
+    @Query('q') q?: string,
+    @Query('bya') bya?: string,
+    @Query('take') take?: number,
+    @Query('skip') skip?: number,
+    @Query('sort') sort?: 'desc' | 'asc',
   ) {
     return this.novelService.findAll({ q, bya, take: take, skip: skip, sort });
   }
